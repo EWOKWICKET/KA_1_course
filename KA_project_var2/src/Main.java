622592660e38de6961640f94b5a8aa0d3d6cbed1
Ivan <oopsgu2006@gmail.com>
2024-03-08 15:52:00 +0200

    Full realisation of project on Java (with correct output)

--- KA_project_var2/src/Main.java
+++ KA_project_var2/src/Main.java
@@ -17,67 +17,90 @@ public class Main {
             String[] numbersStr = input.split("\\s+");
             try {
                 for (String numStr : numbersStr) {
-                    int number = Integer.parseInt(numStr);
+                    long number = Long.parseLong(numStr);
+                    if (number < Integer.MIN_VALUE || number > Integer.MAX_VALUE) {
+                        System.out.printf("Введене число %d виходить за межі допустимого діапазону для int. Використовується максимальне значення: %d\n", number, Integer.MAX_VALUE);
+                        number = Integer.MAX_VALUE;
+                    }
                     System.out.printf("%d\n", number);
-                    addToArray(number);
+                    addToArray((int) number);
                 }
             } catch (NumberFormatException e) {
                 System.err.println("Введено нечислове значення. Спробуйте ще раз.");
             }
         }
-            in.close();
+        in.close();
 
+//        System.out.println(Arrays.toString(numArray).replace("[", "").replace("]", " ").replace(",", ""));
 
-            System.out.println(Arrays.toString(numArray));
-            int[] sortedNumArray = numsMergeSort(numArray);
-            System.out.println(Arrays.toString(sortedNumArray));
+        int[] sortedNumArray = numsMergeSort(numArray);
+//        System.out.println(Arrays.toString(sortedNumArray).replace("[", "").replace("]", " ").replace(",", ""));
+
+        int[] median = median(sortedNumArray);
+        System.out.print("Median: ");
+        System.out.println(Arrays.toString(median).replace("[", "").replace("]", " ").replace(",", ""));
+
+        double avg = average(sortedNumArray);
+        System.out.println("Median: " + avg);
+    }
+
+    public static void addToArray(int num) {
+        numArray = Arrays.copyOf(numArray, numArray.length + 1);
+        numArray[numArray.length - 1] = num;
+    }
+
+    public static int[] numsMergeSort(int[] array) {
+        if (array.length <= 1) {
+            return array;
         }
 
-        public static void addToArray ( int num){
-            numArray = Arrays.copyOf(numArray, numArray.length + 1);
-            numArray[numArray.length - 1] = num;
-        }
+        int[] left = new int[array.length / 2];
+        int[] right = new int[array.length - left.length];
 
-        public static int[] numsMergeSort ( int[] array){
-            if (array.length <= 1) {
-                return array;
-            }
+        System.arraycopy(array, 0, left, 0, left.length);
+        System.arraycopy(array, left.length, right, 0, right.length);
 
-            int[] left = new int[array.length / 2];
-            int[] right = new int[array.length - left.length];
+        left = numsMergeSort(left);
+        right = numsMergeSort(right);
 
-            System.arraycopy(array, 0, left, 0, left.length);
-            System.arraycopy(array, left.length, right, 0, right.length);
+        return numsMerge(left, right);
+    }
 
-            left = numsMergeSort(left);
-            right = numsMergeSort(right);
+    public static int[] numsMerge(int[] left, int[] right) {
+        int[] result = new int[left.length + right.length];
 
-            return numsMerge(left, right);
-        }
+        int leftIndex = 0;
+        int rightIndex = 0;
+        int resultIndex = 0;
 
-        public static int[] numsMerge ( int[] left, int[] right){
-            int[] result = new int[left.length + right.length];
-
-            int leftIndex = 0;
-            int rightIndex = 0;
-            int resultIndex = 0;
-
-            while (leftIndex < left.length && rightIndex < right.length) {
-                if (left[leftIndex] >= right[rightIndex]) {
-                    result[resultIndex++] = right[rightIndex++];
-                } else {
-                    result[resultIndex++] = left[leftIndex++];
-                }
-            }
-
-            while (leftIndex < left.length) {
+        while (leftIndex < left.length && rightIndex < right.length) {
+            if (left[leftIndex] >= right[rightIndex]) {
+                result[resultIndex++] = right[rightIndex++];
+            } else {
                 result[resultIndex++] = left[leftIndex++];
             }
+        }
 
-            while (rightIndex < right.length) {
-                result[resultIndex++] = right[rightIndex++];
-            }
+        while (leftIndex < left.length) {
+            result[resultIndex++] = left[leftIndex++];
+        }
 
-            return result;
+        while (rightIndex < right.length) {
+            result[resultIndex++] = right[rightIndex++];
+        }
+
+        return result;
+    }
+
+    public static int[] median(int[] numArray) {
+        if (numArray.length % 2 == 1) {
+            return Arrays.copyOfRange(numArray, numArray.length / 2, numArray.length / 2 + 1);
+        } else {
+            return Arrays.copyOfRange(numArray, numArray.length / 2 - 1, numArray.length / 2 + 1);
         }
     }
+
+    public static double average(int[] numArray) {
+        return Arrays.stream(numArray).sum() / (double) numArray.length;
+    }
+}
