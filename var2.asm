aa8205d9c1998b64fa0edd51f364f66171ecc8aa
Ivan <oopsgu2006@gmail.com>
2024-03-22 02:14:09 +0200

    filling array and binary nims output(works only with positive nums)

--- var2.asm
+++ var2.asm
@@ -2,18 +2,16 @@
 .stack 100
 
 .data
-numbers dw 10000 dup(?)
+numbers dw 1000 dup(?)          ;масив із числами
 ; numbersAmount dw 0        
-digitsRead dw 0
-numsRead dw 0
-numsConvertedToDecimal dw 0
-numsConvertedToBinary dw 0
-decimalHolder dw 0
-binaryHolder dw 0B
-stackOffset dw 0
-char db  0
-returnIndex dw 0
-; error_string db "Only digits allowed", '$'
+digitsRead dw 0                 ;кількість цифр у числі
+numsRead dw 0                   ;кількість зчитаних з файлу чисел
+numsConvertedToDecimal dw 0     ;кількість жесяткових чисел
+numsConvertedToBinary dw 0      ;кількість двійкових чисел
+decimalHolder dw 0D              ;змінна, що матиме повне конвертоване десяткове число
+char db  0                      ;містить зчитаний символ
+returnIndex dw 0                ;змінна для утримування індексу для повернення до місця виклику функції
+
 
 .code
 main PROC
@@ -28,7 +26,6 @@ main PROC
     call read_loop
     call convert_to_decimal
     call convert_to_binary
-    ; call print_array
     
     jmp end_program
 
@@ -37,7 +34,7 @@ main ENDP
 
 ;reads and prints file, preparing for converting(digits of num and amount of digits are in stack)
 read_loop PROC
-    pop returnIndex
+    pop returnIndex                        ;зберіг ip повернення, бо буду працювати із стеком
     read_file:
         mov ah, 3Fh
         mov bx, 0h                         ; stdin handle
@@ -48,42 +45,43 @@ read_loop PROC
         jz end_of_file
 
         mov dx, 0
-
-        mov ah, 02h
+        ; mov ah, 02h
         mov dl, char
-        int 21h
+        ; int 21h
 
         cmp dl, 13                                      ; LF?
         je read_not_digit
         cmp dl, 10                                      ; CR?
         je read_not_digit
-        cmp dl, ' '
+        cmp dl, ' '                                     ;space?
         je read_not_digit
-        cmp dl, 0
+        cmp dl, 0                                       ;EOF?
         je end_of_file
+        ; cmp dl, '-' 
+        ; je negative
         inc digitsRead
         sub dl, '0'
         push dx
         jmp read_file
-
+    ;якщо прочитана не цифра. Якщо до цього були прочитані цифри, то зупиняє підрахунок цифр у числі
     read_not_digit:
         cmp digitsRead, 0
         jne end_of_num
         jmp read_file
-        
+    ;EOF found
     end_of_file:
         cmp digitsRead, 0
         jne last_num
         push returnIndex
         ret
-
+    ;кладе в стек кількість цифр у числі
     end_of_num:
         push digitsRead
         mov digitsRead, 0
         inc numsRead
         jmp read_file
 
-
+    ;if EOF was met after reading digits
     last_num:
         push digitsRead
         mov digitsRead, 0
@@ -94,23 +92,32 @@ read_loop ENDP
 
 ;converts each num in stack into decimal and pushes them to stack
 convert_to_decimal PROC
-    pop returnIndex
-    mov stackOffset, 0
+    pop returnIndex                                     ;зберіг ip повернення, бо буду працювати із стеком
+    ;переходжу вперед по масиву, щоб зберегти порядок елементів
+    lea si, numbers
+    mov ax, numsRead
+    dec ax
+    mov bx, 2
+    mul bx
+    add si, ax              
+    mov ax, 0
+    mov bx, 0
+    ;перетворення усіх зчитаних символів чисел у десяткові і заповнення масиву
     get_decimal_loop:
         cmp numsRead, 0
         je end_get_decimal_loop
 
-        add sp, stackOffset
-        pop cx
+        pop cx                          ;кількість цифр у числі
         mov bx, 1
+        ;конвертує одне число
         convert_char_loop:
             cmp cx, 0
             je end_convert_char_loop
-            pop dx
+            pop ax
+            ;конвертує цифру числа
             convert_char:
-                mov ax, dx
                 mul bx
-                add decimalHolder, ax
+                add decimalHolder, ax           ;додаю число домножене на певну степінь 10 до змінної, в якій лежатиме повне число
                 mov ax, bx
                 mov bx, 10
                 mul bx
@@ -118,16 +125,16 @@ convert_to_decimal PROC
                 dec cx
                 jmp convert_char_loop
 
-
+        ;кінець конвертації числа і його занесення в масив
         end_convert_char_loop:
-            push decimalHolder
-            mov decimalHolder, 0
-            sub sp, stackOffset
-            add stackOffset, 2
+            mov ax, decimalHolder
+            mov [numbers + si], ax
+            sub si, 2
+            mov decimalHolder, 0D
             inc numsConvertedToDecimal
             dec numsRead 
             jmp get_decimal_loop
-
+    
     end_get_decimal_loop:
         push returnIndex
         ret    
@@ -135,267 +142,65 @@ convert_to_decimal ENDP
 
 ;converts each decimal in stack into binary and pushes them to stack
 convert_to_binary PROC
-    pop returnIndex
-    mov stackOffset, 0
-    mov digitsRead, 0
-    mov bx, 2
+    pop returnIndex                         ;зберіг ip повернення, бо буду працювати із стеком
+    lea si, numbers
     get_binary_loop:
         cmp numsConvertedToDecimal, 0
-        je end_get_decimal_loop
-        add sp, stackOffset
-        pop dx
+        je end_get_binary_loop
+        mov ax, [numbers + si]
+        add si, 2                   ;збільшення індексу для робоит з масивом
+        mov digitsRead, 0D          
+        mov bx, 2                   ;дільник
         convert_digit_loop:
-            mov ax, dx
             mov dx, 0
-            mov ah, 0
-
-            div bl
-            mov dl, ah
-            mov ah, 0
-            push dx
+            div bx
+            push dx                 ;остача
             inc digitsRead
 
-            cmp ax, 0
-            je convert_digit
+            cmp ax, 0               ;перевіряю частку
+            je full_convert
             jmp convert_digit_loop
+            ;число в доповняльному коді(аоки що тільки додатні)
+            full_convert:
+                mov bx, digitsRead
+                mov cx, 16
+                sub cx, bx
+                ;доповнляьний код
+                additional_code:
+                    cmp cx, 0
+                    je remainders_gathering
+                    mov ah, 02h
+                    mov dx, '0'
+                    int 21h
+                    dec cx
+                    jmp additional_code
+                ;основне число
+                remainders_gathering:
+                    cmp digitsRead, 0
+                    je end_convert_digit_loop
+                    pop dx
+                    mov ah, 02h
+                    add dx, '0'
+                    int 21h
+                    dec digitsRead
+                    jmp remainders_gathering
 
-            convert_digit:
-                cmp digitsRead, 0
-                je end_convert_digit_loop
-                pop dx
-                mov ah, 02h
-                add dl, '0'
-                int 21h
-                sub dl, '0'
-                dec digitsRead
-                jmp convert_digit
-
+        ;розділення між числами
         end_convert_digit_loop:
-            sub sp, stackOffset
-            add stackOffset, 2
+            mov ah, 02h
+            mov dx, ' '
+            int 21h
             inc numsConvertedToBinary
             dec numsConvertedToDecimal 
-            jmp get_decimal_loop
+            jmp get_binary_loop
 
     end_get_binary_loop:
-        mov stackOffset, 0
         push returnIndex
         ret    
 convert_to_binary ENDP
-; addToNumbers PROC      
-;     mov cx, 0
-;     mov dx, 0
-;     mov bx, 0
-
-;     get_digit_loop:
-;         push cx
-;         mov ah, 3Fh
-;         mov bx, 0h                         ; stdin handle
-;         mov cx, 1                          ; 1 byte to read
-;         lea dx, char                       ; read to ds:dx
-;         int 21h  
-
-;         cmp ax, 0                                        ; EOF?
-;         je found_EOF  
-;         mov cx, 0
-;         pop cx
-;         mov dl, char
-;         cmp dl, 0Ah                                      ; LF?
-;         je digits_ended  
-;         cmp dl, 0Dh                                      ; CR?
-;         je digits_ended                      
-;         cmp dl, ' '                                      ; пробіл?
-;         je digits_ended               
-;         ; cmp dl, '-'
-;         ; jz mark_as_negative   
-
-;         ; cmp dl, '0'                                      ; не цифра?
-;         ; jb digits_ended       
-;         ; cmp dl, '9'                                      ; не цифра?
-;         ; ja digits_ended
-;         jmp add_to_digits
-
-;     ; mark_as_negative:
-;     ;     push 1111111111111111B
-
-;     add_to_digits:
-;         ; push dx                ; Якщо це цифра, додати її до стеку
-;         inc cx
-;         mov ah, 02h
-;         mov dl, '4'
-;         int 21h
-;         jmp get_digit_loop     
-
-;     ; not_digit:
-;     ;     ; Виводимо повідомлення про помилку
-;     ;     mov ax, 0
-;     ;     lea dx, error_string   
-;     ;     mov ah, 09h            ; вивід рядка   
-;     ;     int 21h                
-;     ;     ; Завершуємо програму       
-;     ;     mov ah, 4Ch           
-;     ;     int 21h                
-
-;     digits_ended: 
-;         ; cmp cx, 0
-;         ; je get_digit_loop
-;         ; ; call convert
-;         mov cx, 0
-;         mov ah, 02h
-;         mov dl, '9'
-;         int 21h
-;         jmp get_digit_loop
-
-;     found_EOF:
-;         ; pop cx
-;         ; cmp cx, 0
-;         ; jnz EOF_convert
-;         ret
-
-;     EOF_convert:
-;         ; call convert
-;         ret
-
-; addToNumbers ENDP
-
-; convert PROC
-;         mov dl, 0             ; Очищення регістру DX (для зберігання результату)
-;         mov bl, 1D
-
-;     convert_loop:
-;         cmp cx, 0               ; Перевірка, чи є ще цифри у стеці
-;         jz end_convert          
-
-;         pop ax
-;         sub ax, '0'             ; Конвертація у числове значення
-;         mul bx
-;         add dx, ax
-;         mov ax, bx
-;         mov bx, 10D
-;         mul bx
-;         mov bx, ax
-
-;         dec cx                  ; Зменшення лічильника цифр у стеці
-;         jmp convert_loop        
-
-;     end_convert:
-;         ; call convert_to_binary        ;FFFFFIIIIIIIIXXXXXXX
-;         ; mov [numbers + si], ax
-;         ; add si, 2
-;         ; inc numbersAmount
-;         ret
-; convert ENDP
-
-; convert_to_binary PROC                          ;ПРОБЛЕМИ ІЗ ЗАЛИШКОМ МОЩНІ ДУЖЕ ЖЕСТЬ ЖОПА;ПРОБЛЕМИ ІЗ ЗАЛИШКОМ МОЩНІ ДУЖЕ ЖЕСТЬ ЖОПА;ПРОБЛЕМИ ІЗ ЗАЛИШКОМ МОЩНІ ДУЖЕ ЖЕСТЬ ЖОПА;ПРОБЛЕМИ ІЗ ЗАЛИШКОМ МОЩНІ ДУЖЕ ЖЕСТЬ ЖОПА
-;     mov ax, dx
-;     mov bl, 2D       ; число для поділу(двійкова система)
-;     mov cx, 0      
-;     jmp convert_loop_to_binary      
-    
-;     convert_loop_to_binary:
-;         cbw
-;         mov dx, 0
-;         div bl               ; Ділимо AX на BX, результат зберігається у AL, а залишок у AH
-;         mov dl, ah
-    
-
-;         ; add dl, '0'
-;         ; mov ah, 02h
-;         ; int 21h
-;         ; mov ah, 0
-;         ; sub dl, '0'
-
-;         push dx              ; Зберегти залишок (0 або 1) у стек
-;         inc cx
-
-;         cmp al, 0                       ; Перевірка, чи AX = 0 (все число поділене)
-;         jz prepare_for_gathering              
-;         jmp convert_loop_to_binary      
-
-;     end_convert_loop:
-;         push ax
-;         mov dl, ' '
-;         mov ah, 02h
-;         int 21h
-;         pop ax
-;         ret
-
-;     prepare_for_gathering:
-;         ; mov bx, 1            ; Початкова степінь двійки (1, 2, 4, 8, ...)
-
-;         ; mov ax, cx
-;         ; mul 2
-;         ; sub bp, ax
-;         ; pop bx
-;         ; add bp, ax
-;         ; mov ax, bx
-;         mov al, 0B                                      ;SET AX 0 FOR TIME BEING. THEN ADD CHECK ON NEGATIVES
-;         cwd
-;         jmp check_if_out_of_limit
-
-;     check_if_out_of_limit:
-;         cmp cl, 15
-;         jbe gather_remainders
-
-;         cmp ax, 0
-;         jns set_max_positive
-;         jmp set_min_negative
-
-;         set_max_positive:
-;             mov ax, 0111111111111111B
-;             jmp end_convert_loop
-
-;         set_min_negative:
-;             mov ax, 1000000000000001B
-;             jmp end_convert_loop
-        
-;     gather_remainders:
-;         cmp cl, 0             ; Перевірка, чи всі залишки зібрано
-;         jz end_convert_loop         
-
-;         pop dx                ; Вибірка залишку зі стека у регістр DX
-;         ; push ax
-;         ; mov ax, dx
-;         ; mul bx               ;множу на поточну степінь двійки
-;         ; mov dx, ax
-;         ; pop ax
-;         shl dx, cl
-;         add ax, dx            ; Додавання поточного результату до нового залишку
-
-;         ; shl bx, 1             ; Подвоєння степеня двійки
-;         dec cl                ; Зменшення лічильника залишків
-;         jmp gather_remainders       
-        
-; convert_to_binary ENDP
 
 
-; print_array PROC
-;     lea si, numbers
-;     mov ah, 09h            ;    вивід рядка
-
-;     display_loop: 
-;         mov dx, [numbers + si]         
-;         cmp dx, ''
-;         jz finish_display
-
-;         call print_number     
-;         add si, 2     
-;         jmp display_loop
-        
-;     finish_display:
-;         mov ax, 0
-;         ret
-; print_array ENDP
-
-; print_number PROC
-;     mov dl, '2'
-;     mov ax, 0
-;     mov ah, 09h
-;     int 21h
-;     ret
-; print_number ENDP
-
-;description
+;завершення програми
 end_program PROC
     mov ax, 0
     mov bx, 0
